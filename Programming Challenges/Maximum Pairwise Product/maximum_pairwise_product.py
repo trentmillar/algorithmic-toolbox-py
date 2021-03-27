bc1d7247edb6ab4cab0c1f27204d4e43cd9a26cb
trentmillar <trent.millar@gmail.com>
2021-03-26 19:52:57 -0600

    initial commit

---
+++ Programming Challenges/Maximum Pairwise Product/maximum_pairwise_product.py
@@ -0,0 +1,39 @@
+# python3
+
+def max_pairwise_product_naive(numbers):
+    assert len(numbers) >= 2
+    assert all(0 <= x <= 2 * 10 ** 5 for x in numbers)
+
+    product = 0
+
+    for i in range(len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            product = max(product, numbers[i] * numbers[j])
+
+    return product
+
+
+def max_pairwise_product(numbers):
+    assert len(numbers) >= 2
+    assert all(0 <= x <= 2 * 10 ** 5 for x in numbers)
+
+    highest = 0
+    next_highest = 0
+
+    for number in numbers:
+        if number > highest:
+            next_highest = highest
+            highest = number
+            continue
+
+        if number > next_highest:
+            next_highest = number
+
+    return next_highest * highest
+
+
+if __name__ == '__main__':
+    n = int(input())
+    input_numbers = list(map(int, input().split()))
+    assert len(input_numbers) == n
+    print(max_pairwise_product(input_numbers))
